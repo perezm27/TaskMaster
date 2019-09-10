2034a56eadb8f67f8261c9aa6bd3b25f035a61a2
MELFI PEREZ <perezmelfi@outlook.com>
2019-09-10 09:39:00 -0700

    application scaffolding and gitignore added

---
+++ README.md
@@ -0,0 +1,7 @@
+## Lab 26: Implementation TaskMaster with NoSQL and DynamoDB
+TaskMaster is a task-tracking application with the same basic goal as Trello: 
+allow users to keep track of tasks to be done and their status.
+
+## Deployed Application 
+
+## Issues encountered during deployment
---
+++ src/main/java/com/perezm27/Taskmaster/TaskmasterApplication.java
@@ -0,0 +1,13 @@
+package com.perezm27.Taskmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmasterApplication.class, args);
+	}
+
+}
