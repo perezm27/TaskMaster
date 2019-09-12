346f7a49fc932eb3e3285be81852d0c59d82d237
MELFI PEREZ <perezmelfi@outlook.com>
2019-09-11 23:46:13 -0700

    refactored, now able to retrieve data from Dynamo without converter

--- src/main/java/com/perezm27/Taskmaster/Repository/TaskRepository.java
+++ src/main/java/com/perezm27/Taskmaster/Repository/TaskRepository.java
@@ -4,9 +4,11 @@ import com.perezm27.Taskmaster.Models.Task;
 import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 @EnableScan
 public interface TaskRepository extends CrudRepository<Task, String> {
         Optional<Task> findById(String id);
+        List<Task> findByAssignee(String name);
 }
